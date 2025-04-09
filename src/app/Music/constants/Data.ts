5b4d59eae3311aea1839c8b23fdccbc28f60d489
Amr-7246 <aehab7246@gmail.com>
2025-04-09 22:01:51 +0200

    Done

--- src/app/Music/constants/Data.ts
+++ src/app/Music/constants/Data.ts
@@ -1,4 +1,26 @@
-import { photo1, photo2, photo3, photo4, photo5, photo6 } from "../../../assets/music/image/index";
+import {
+  photo1,
+  photo2,
+  photo3,
+  photo4,
+  photo5,
+  photo6,
+  photo7,
+  photo8,
+  photo9,
+  photo10,
+  photo11,
+  photo12,
+  photo13,
+  photo14,
+  photo15,
+  photo16,
+  photo17,
+  photo18,
+  photo19,
+  photo20,
+} from "../../../assets/music/image/index";
+
 import { MusicItem } from "../types/music";
 
 export const musicData: MusicItem[] = [
@@ -48,70 +70,133 @@ export const musicData: MusicItem[] = [
     id: "7",
     title: "سورة ق كاملة",
     description: "بصوت ياسر ممدوح | تلاوة مؤثرة",
-    image: photo1,
+    image: photo7,
     audio: "/assets/sound/surahQComplete.mp3"
   },
   {
     id: "8",
     title: "سورة الغاشية",
     description: "بصوت الشيخ محمد صديق المنشاوي",
-    image: photo2,
+    image: photo8,
     audio: "/assets/sound/surahAlGhashiya.mp3"
   },
   {
     id: "9",
     title: "سورة الفجر",
     description: "تلاوة نادرة جداً للمنشاوي",
-    image: photo3,
+    image: photo9,
     audio: "/assets/sound/surahAlFajr.mp3"
   },
   {
     id: "10",
     title: "سورة القمر",
     description: "محمود علي البنا | أداء مذهل",
-    image: photo4,
+    image: photo10,
     audio: "/assets/sound/surahAlQamar.mp3"
   },
   {
     id: "11",
     title: "سورة النازعات",
     description: "الشيخ المنشاوي بدون إعلانات",
-    image: photo5,
+    image: photo11,
     audio: "/assets/sound/surahAlNaziat.mp3"
   },
   {
     id: "12",
     title: "الصوت الباكي",
     description: "الشيخ المنشاوي في أروع تلاوة",
-    image: photo6,
+    image: photo12,
     audio: "/assets/sound/alSoutAlBaki.mp3"
   },
   {
     id: "13",
     title: "سورة الحاقة",
     description: "تجويد نادر بصوت المنشاوي",
-    image: photo1,
+    image: photo13,
     audio: "/assets/sound/surahAlHaqqa.mp3"
   },
   {
     id: "14",
     title: "ابتهال الإذاعة",
     description: "أخيرًا صورة وصوت بعد طول انتظار",
-    image: photo2,
+    image: photo14,
     audio: "/assets/sound/ibtihalRadio.mp3"
   },
   {
     id: "15",
     title: "حسبنا الله ونعم الوكيل",
     description: "الشيخ المنشاوي بخشوع مؤثر",
-    image: photo3,
+    image: photo15,
     audio: "/assets/sound/hasbunAllahu.mp3"
   },
   {
     id: "16",
     title: "ولا تقولن لشيء",
     description: "الشيخ المنشاوي يتلو آيات عظيمة",
-    image: photo4,
+    image: photo16,
     audio: "/assets/sound/laTaqulunLishay.mp3"
+  },
+  {
+    id: "17",
+    title: "سورة القيامة بصوت مبكٍ",
+    description: "ادريس أبكر بتلاوة مؤثرة تلامس القلب ",
+    image: photo17,
+    audio: "/assets/sound/_ادريس_أبكر_،_سورة_القيامة_صوت_مبكى_لأبعد_الحدوووودMP3_160K.mp3"
+  },
+  {
+    id: "18",
+    title: "الفاتحة وفواتح البقرة",
+    description: "المنشاوي يبدع بكل القراءات في محفل أسطوري ",
+    image: photo18,
+    audio: "/assets/sound/الفاتحة_و_فواتح_البقرة_بكل_القراءات_للشيخ_المنشاوي_محفل_اسطوريMP3.mp3"
+  },
+  {
+    id: "19",
+    title: "بكاء المنشاوي",
+    description: "تلاوة نادرة تجسد مناجاة صادقة ومؤثرة ",
+    image: photo19,
+    audio: "/assets/sound/بكاء المنشاوي وهو يناجي ربه(MP3_160K).mp3"
+  },
+  {
+    id: "20",
+    title: "سورة الطور",
+    description: "محمود علي البنا بصوت مميز وقراءة خاشعة ",
+    image: photo20,
+    audio: "/assets/sound/سورة الطور - محمود علي البنا(MP3_160K).mp3"
+  },
+  {
+    id: "21",
+    title: "قصار السور",
+    description: "تجميعة لتلاوات خاشعة للشيخ محمود علي البنا ",
+    image: photo1,
+    audio: "/assets/sound/قصار_السور_الشيخ_محمود_على_البناMP3_160K.mp3"
+  },
+  {
+    id: "22",
+    title: "قصة سيدنا إبراهيم",
+    description: "المنشاوي يصور القصة كأنك تراها",
+    image: photo2,
+    audio: "/assets/sound/قصة_سيدنا_إبراهيم_مع_أبيه_،_يصورها_الشيخ_المنشاوي_كأنما_يراها_MP3.mp3"
+  },
+  {
+    id: "23",
+    title: "تلاوة تفوق الوصف",
+    description: "الشيخ المنشاوي يتلو سورة التكوير بإبداع رهيب ",
+    image: photo3,
+    audio: "/assets/sound/محمد_صديق_المنشاوي_تلاوة_تفوق_الوصف_لسورة_التكويرMP3_160K.mp3"
+  },
+  {
+    id: "24",
+    title: "وَقَالُوا حَسْبُنَا اللَّهُ",
+    description: "تلاوة رائعة للآية بصوت المنشاوي ",
+    image: photo4,
+    audio: "/assets/sound/وَقَالُوا_حَسْبُنَا_اللَّهُ_وَنِعْمَ_الْوَكِيلُمحمد_صديق_المنشاوي.mp3"
+  },
+  {
+    id: "25",
+    title: "سورة عبس كاملة",
+    description: "يَوْمَ يَفِرُّ الْمَرْءُ... تلاوة تلامس الروح",
+    image: photo5,
+    audio: "/assets/sound/يَوْمَ_يَفِرُّ_الْمَرْءُ_مِنْ_أخِيهِ_وأُمِّهِ_وأبِيهِ_سورة_عبس_كاملة.mp3"
   }
 ];
--- src/assets/music/image/index.ts
+++ src/assets/music/image/index.ts
@@ -4,5 +4,40 @@ import photo3 from "./photo_3_2025-04-05_01-50-10.jpg";
 import photo4 from "./photo_4_2025-04-05_01-50-11.jpg";
 import photo5 from "./photo_5_2025-04-05_01-50-11.jpg";
 import photo6 from "./photo_6_2025-04-05_01-50-11.jpg";
+import photo7 from "./photo_1_2025-04-09_21-13-34.jpg";
+import photo8 from "./photo_2_2025-04-09_21-13-34.jpg";
+import photo9 from "./photo_3_2025-04-09_21-13-34.jpg";
+import photo10 from "./photo_4_2025-04-09_21-13-35.jpg";
+import photo11 from "./photo_5_2025-04-09_21-13-35.jpg";
+import photo12 from "./photo_6_2025-04-09_21-13-35.jpg";
+import photo13 from "./photo_7_2025-04-09_21-13-35.jpg";
+import photo14 from "./photo_8_2025-04-09_21-13-35.jpg";
+import photo15 from "./photo_9_2025-04-09_21-13-35.jpg";
+import photo16 from "./photo_10_2025-04-09_21-13-35.jpg";
+import photo17 from "./photo_11_2025-04-09_21-13-35.jpg";
+import photo18 from "./photo_12_2025-04-09_21-13-35.jpg";
+import photo19 from "./photo_13_2025-04-09_21-13-35.jpg";
+import photo20 from "./photo_14_2025-04-09_21-13-35.jpg";
 
-export { photo1, photo2, photo3, photo4, photo5, photo6 };
+export {
+    photo1,
+    photo2,
+    photo3,
+    photo4,
+    photo5,
+    photo6,
+    photo7,
+    photo8,
+    photo9,
+    photo10,
+    photo11,
+    photo12,
+    photo13,
+    photo14,
+    photo15,
+    photo16,
+    photo17,
+    photo18,
+    photo19,
+    photo20,
+};
